5c0fa230e22ee57c2fc9157d2d5d37cc9833f8cf
Youness Dominique <youness.dominique@gmail.com>
2023-04-23 18:04:36 +0200

    [FEAT] Update footer information

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,14 +1,14 @@
+
 <footer class="footer-wrapper footer-layout2" data-bg-src="assets/img/bg/footer-bg-2-1.jpg">
     <div class="footer-top">
         <div class="container">
             <div class="row align-items-center justify-content-between gy-30">
                 <div class="col-sm-auto">
-                    <div class="footer-top-logo">
-                        <a href="{{route('home')}}"><img src="assets/img/logo-white.png" alt="Manne Cachée"
+                    <div class="footer-top-logo"><a href="index.html"><img src="assets/img/logo-white.png" alt="TechBiz"
                                                                            class="logo"></a></div>
                 </div>
                 <div class="col-sm-auto">
-                    <div class="footer-social"><span class="social-title">Suivez-nous sur :</span>
+                    <div class="footer-social"><span class="social-title">Suivez-nous sur:</span>
                         <a href="#"><i
                                 class="fab fa-facebook-f"></i></a>
                         <a href="#"><i class="fab fa-twitter"></i></a>
@@ -26,67 +26,53 @@
                     <div class="widget widget_nav_menu footer-widget"><h3 class="widget_title">Liens rapides</h3>
                         <div class="menu-all-pages-container footer-links">
                             <ul class="menu">
-                                <li><a href="#">A propos</a></li>
-                                <li><a href="#">Our Mission</a></li>
-                                <li><a href="#">Meet The Teams</a></li>
-                                <li><a href="#">Our Projects</a></li>
-                                <li><a href="#">Contact Us</a></li>
-                                <li><a href="#">Our Services</a></li>
-                                <li><a href="#">About Us</a></li>
-                                <li><a href="#">Our Mission</a></li>
-                                <li><a href="#">Meet The Teams</a></li>
-                                <li><a href="#">Our Projects</a></li>
-                                <li><a href="#">Contact Us</a></li>
+                                <li><a href="#">A Propos</a></li>
+                                <li><a href="#">Notre mission</a></li>
+                                <li><a href="#">Notre équipe</a></li>
+                                <li><a href="#">Nos projets</a></li>
+                                <li><a href="#">Nous contactez</a></li>
                             </ul>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-xl-auto order-1 order-lg-2">
-                    <div class="widget footer-widget"><h3 class="widget_title">About Company</h3>
+                    <div class="widget footer-widget"><h3 class="widget_title">A Propos de nous</h3>
                         <div class="vs-widget-about"><p class="footer-text">Continually evolve worldwide vortals rather
                                 than process centric human capital.wide benefits via quality niches provide access without
                                 plagiarize collaborative alignments after</p>
-                            <form action="#" class="footer-newsletter"><input type="email"
-                                                                              placeholder="Enter Email Address">
-                                <button type="submit" class="vs-btn style2">Subscribe</button>
-                            </form>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-4 col-xl-auto order-3">
-                    <div class="widget footer-widget"><h3 class="widget_title">Working Hours</h3>
+                    <div class="widget footer-widget"><h3 class="widget_title">Heure de travail</h3>
                         <div class="footer-schedule">
                             <table>
                                 <tbody>
                                 <tr>
-                                    <th>Mone - Fri:</th>
-                                    <td>09:00 - 06:00</td>
+                                    <th>Lundi - Vendredi:</th>
+                                    <td>08:00 - 16:00</td>
                                 </tr>
                                 <tr>
-                                    <th>Saturday:</th>
-                                    <td>09:00 - 12:00</td>
+                                    <th>Samedi:</th>
+                                    <td>08:00 - 12:00</td>
                                 </tr>
                                 <tr>
-                                    <th>Sunday</th>
-                                    <td>Closed</td>
+                                    <th>Dimanche</th>
+                                    <td>Fermé</td>
                                 </tr>
                                 </tbody>
                             </table>
                         </div>
-                        <div class="footer-number"><h4 class="title h6">Need Help? Contact Number</h4>
-                            <a class="info" href="tel:+243811497046">+243811497046</a> /
-                            <a class="info" href="tel:+243972043209">+243972043209</a>
-                        </div>
+                        <div class="footer-number"><h4 class="title h6">Plus d'information? Contactez-vous:</h4><a
+                                href="tel:+243972043209" class="info">+243972043209</a> | <a href="tel: +243811497046" class="info"> +243811497046</a></div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <div class="copyright-wrap">
-        <div class="container"><p class="copyright-text">Copyright <i class="fal fa-copyright"></i> 2022 <a
-                    class="text-white" href="{{route('home')}}">Groupe Manne Cachée</a>. All rights reserved by
-                <a class="text-white"
-                   href="https://domiyns.com">domiyns</a>.
+        <div class="container"><p class="copyright-text">Copyright <i class="fal fa-copyright"></i> {{\Carbon\Carbon::parse(\Carbon\Carbon::now())->format('Y')}} <a
+                    class="text-white" href="{{route('home')}}">Groupe Manne Cachée</a>. All rights reserved.
             </p></div>
     </div>
 </footer>
