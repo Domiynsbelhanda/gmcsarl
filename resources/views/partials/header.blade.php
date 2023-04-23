44f5db6da963bd48ddd3ad6d412ecd205ff213f9
Youness Dominique <youness.dominique@gmail.com>
2023-04-23 16:48:02 +0200

    [STYLE] Update header information

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,71 +1,35 @@
 <div class="vs-menu-wrapper">
     <div class="vs-menu-area text-center">
         <button class="vs-menu-toggle"><i class="fal fa-times"></i></button>
-        <div class="mobile-logo"><a href="index.html"><img src="assets/img/logo.png" alt="TechBiz" class="logo"></a>
+        <div class="mobile-logo"><a href="{{route('home')}}"><img src="assets/img/logo.png" alt="Manne Cachée" class="logo"></a>
         </div>
         <div class="vs-mobile-menu">
             <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="about.html">About Us</a></li>
-                <li class="menu-item-has-children"><a href="blog.html">Blog</a>
-                    <ul class="sub-menu">
-                        <li><a href="blog.html">Blog List</a></li>
-                        <li><a href="blog-grid.html">Blog Grid</a></li>
-                        <li><a href="blog-details.html">Blog Details</a></li>
-                    </ul>
-                </li>
-                <li class="menu-item-has-children"><a href="#none">Pages</a>
-                    <ul class="sub-menu">
-                        <li><a href="price-plan.html">Pricing Plans</a></li>
-                        <li><a href="project.html">Projects</a></li>
-                        <li><a href="project-details.html">Projects Details</a></li>
-                        <li><a href="service.html">Service</a></li>
-                        <li><a href="service-details.html">Service Details</a></li>
-                        <li><a href="team.html">Team</a></li>
-                        <li><a href="team-details.html">Team Details</a></li>
-                    </ul>
-                </li>
-                <li class="menu-item-has-children"><a href="#none"><span class="has-new-lable">Elements<span
-                                class="new-label">new</span></span></a>
-                    <ul class="sub-menu">
-                        <li><a href="element-typography.html">Typography</a></li>
-                        <li><a href="element-buttons.html">Buttons</a></li>
-                        <li><a href="element-columns.html">Columns</a></li>
-                        <li><a href="element-messagebox.html">Message Box</a></li>
-                        <li><a href="element-separators.html">Separators</a></li>
-                        <li><a href="element-services.html">Services Card</a></li>
-                        <li><a href="element-testimonials.html">Testimonials</a></li>
-                        <li><a href="element-projectbox.html">Project Box</a></li>
-                        <li><a href="element-priceplan.html">Price Plan</a></li>
-                        <li><a href="element-counters.html">Counters</a></li>
-                        <li><a href="element-accordions.html">Accordions</a></li>
-                        <li><a href="element-team.html">Team</a></li>
-                        <li><a href="element-process.html">Process</a></li>
-                        <li><a href="element-blogcard.html">Blog Card</a></li>
-                        <li><a href="element-ctas.html">Call To Actions</a></li>
-                        <li><a href="element-map.html">Google Map</a></li>
-                    </ul>
-                </li>
-                <li><a href="contact.html">Contact Us</a></li>
+                <li><a href="{{route('home')}}">Accueil</a></li>
+                <li><a href="">A Propos de nous</a></li>
+                <li><a href="">Services</a></li>
+                <li><a href="">Contactez-nous</a></li>
             </ul>
         </div>
     </div>
 </div>
+
 <div class="sidemenu-wrapper d-none d-lg-block">
     <div class="sidemenu-content">
         <button class="closeButton sideMenuCls"><i class="far fa-times"></i></button>
         <div class="widget">
             <div class="vs-widget-about">
-                <div class="footer-logo"><a href="index.html"><img src="assets/img/logo.png" alt="TechBiz" class="logo"></a>
+                <div class="footer-logo"><a href="{{route('home')}}"><img src="assets/img/logo.png" alt="Manne Cachée" class="logo"></a>
                 </div>
-                <p class="footer-text">Intrinsicly evisculate emerging cutting edge scenarios redefine future-proof
-                    e-markets demand line</p>
-                <div class="footer-social"><a href="#"><i class="fab fa-facebook-f"></i></a> <a href="#"><i
-                            class="fab fa-twitter"></i></a> <a href="#"><i class="fab fa-instagram"></i></a> <a href="#"><i
-                            class="fab fa-behance"></i></a> <a href="#"><i class="fab fa-youtube"></i></a></div>
+                <div class="footer-social">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i
+                            class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </div>
             </div>
         </div>
-        <div class="widget"><h4 class="widget_title">Gallery Posts</h4>
+        <div class="widget"><h4 class="widget_title">Notre Gallerie</h4>
             <div class="sidebar-gallery">
                 <div class="gallery-thumb"><img src="assets/img/widget/gal-1-1.jpg" alt="Gallery Image" class="w-100">
                 </div>
@@ -91,6 +55,7 @@
         </div>
     </div>
 </div>
+
 <header class="vs-header header-layout2">
     <div class="header-shape"></div>
     <div class="header-top">
@@ -129,135 +94,23 @@
                 <div class="menu-area">
                     <div class="row align-items-center justify-content-between">
                         <div class="col-auto">
-                            <div class="logo-style1"><a href="index.html"><img src="assets/img/logo-white.png"
-                                                                               alt="TechBiz" class="logo"></a></div>
+                            <div class="logo-style1"><a href="{{route('home')}}"><img src="assets/img/logo-white.png"
+                                                                               alt="Manne Cachée" class="logo"></a></div>
                         </div>
                         <div class="col-auto">
                             <nav class="main-menu menu-style2 d-none d-lg-block">
                                 <ul>
-                                    <li class="menu-item-has-children"><a href="index.html"><span class="has-new-lable">Home<span
-                                                    class="new-label">new</span></span></a>
-                                        <ul class="sub-menu">
-                                            <li><a href="index.html">Home 1 - Layerslider</a></li>
-                                            <li><a href="index-slickslider.html">Home 1 - Slickslider <span
-                                                        class="new-label">new</span></a></li>
-                                            <li><a href="index-2.html">Home 2 - Layerslider</a></li>
-                                            <li><a href="index-2-slickslider.html">Home 2 - Slickslider <span
-                                                        class="new-label">new</span></a></li>
-                                            <li><a href="index-3.html">Home 3 - Layerslider</a></li>
-                                            <li><a href="index-3-slickslider.html">Home 3 - Slickslider <span
-                                                        class="new-label">new</span></a></li>
-                                            <li><a href="index-4.html">Home 4 - Layerslider <span
-                                                        class="new-label">new</span></a></li>
-                                            <li><a href="index-4-slickslider.html">Home 4 - Slickslider <span
-                                                        class="new-label">new</span></a></li>
-                                            <li><a href="index-5.html">Home 5 - Layerslider <span
-                                                        class="new-label">new</span></a></li>
-                                            <li><a href="index-5-slickslider.html">Home 5 - Slickslider <span
-                                                        class="new-label">new</span></a></li>
-                                        </ul>
+                                    <li><a href="{{route('home')}}">Accueil</a>
                                     </li>
-                                    <li><a href="about.html">About Us</a></li>
-                                    <li class="menu-item-has-children"><a href="service.html">Service</a>
-                                        <ul class="sub-menu">
-                                            <li><a href="service.html">Service</a></li>
-                                            <li><a href="service-details.html">Service Details</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="menu-item-has-children mega-menu-wrap"><a href="#">Pages</a>
-                                        <ul class="mega-menu">
-                                            <li><a href="shop.html">Pagelist 1</a>
-                                                <ul>
-                                                    <li><a href="index.html">Home 1 - Layerslider</a></li>
-                                                    <li><a href="index-slickslider.html">Home 1 - Slickslider <span
-                                                                class="new-label">new</span></a></li>
-                                                    <li><a href="index-2.html">Home 2 - Layerslider</a></li>
-                                                    <li><a href="index-2-slickslider.html">Home 2 - Slickslider <span
-                                                                class="new-label">new</span></a></li>
-                                                    <li><a href="index-3.html">Home 3 - Layerslider</a></li>
-                                                    <li><a href="index-3-slickslider.html">Home 3 - Slickslider <span
-                                                                class="new-label">new</span></a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="#">Pagelist 2</a>
-                                                <ul>
-                                                    <li><a href="index-4.html">Home 4 - Layerslider <span
-                                                                class="new-label">new</span></a></li>
-                                                    <li><a href="index-4-slickslider.html">Home 4 - Slickslider <span
-                                                                class="new-label">new</span></a></li>
-                                                    <li><a href="index-5.html">Home 5 - Layerslider <span
-                                                                class="new-label">new</span></a></li>
-                                                    <li><a href="index-5-slickslider.html">Home 5 - Slickslider <span
-                                                                class="new-label">new</span></a></li>
-                                                    <li><a href="about.html">About Us</a></li>
-                                                    <li><a href="price-plan.html">Pricing Plans</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="#">Pagelist 3</a>
-                                                <ul>
-                                                    <li><a href="blog.html">Blog List</a></li>
-                                                    <li><a href="blog-grid.html">Blog Grid</a></li>
-                                                    <li><a href="blog-details.html">Blog Details</a></li>
-                                                    <li><a href="service.html">Service</a></li>
-                                                    <li><a href="service-details.html">Service Details</a></li>
-                                                    <li><a href="team.html">Team</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="#">Pagelist 4</a>
-                                                <ul>
-                                                    <li><a href="team-details.html">Team Details</a></li>
-                                                    <li><a href="project.html">Projects</a></li>
-                                                    <li><a href="project-details.html">Projects Details</a></li>
-                                                    <li><a href="contact.html">Contact Us</a></li>
-                                                    <li><a href="error.html">Error Page</a></li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li class="menu-item-has-children mega-menu-wrap"><a href="#"><span
-                                                class="has-new-lable">Elements<span class="new-label">new</span></span></a>
-                                        <ul class="mega-menu">
-                                            <li><a href="shop.html">Elements</a>
-                                                <ul>
-                                                    <li><a href="element-typography.html">Typography</a></li>
-                                                    <li><a href="element-buttons.html">Buttons</a></li>
-                                                    <li><a href="element-columns.html">Columns</a></li>
-                                                    <li><a href="element-messagebox.html">Message Box</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="#">Elements</a>
-                                                <ul>
-                                                    <li><a href="element-separators.html">Separators</a></li>
-                                                    <li><a href="element-services.html">Services Card</a></li>
-                                                    <li><a href="element-testimonials.html">Testimonials</a></li>
-                                                    <li><a href="element-projectbox.html">Project Box</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="#">Elements</a>
-                                                <ul>
-                                                    <li><a href="element-priceplan.html">Price Plan</a></li>
-                                                    <li><a href="element-counters.html">Counters</a></li>
-                                                    <li><a href="element-accordions.html">Accordions</a></li>
-                                                    <li><a href="element-team.html">Team</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="#">Elements</a>
-                                                <ul>
-                                                    <li><a href="element-process.html">Process</a></li>
-                                                    <li><a href="element-blogcard.html">Blog Card</a></li>
-                                                    <li><a href="element-ctas.html">Call To Actions</a></li>
-                                                    <li><a href="element-map.html">Google Map</a></li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="contact.html">Contact</a></li>
+                                    <li><a href="">A propos de nous</a></li>
+                                    <li><a href="">Services</a></li>
+                                    <li><a href="">Contactez-nous</a></li>
                                 </ul>
                             </nav>
                             <button class="vs-menu-toggle d-inline-block d-lg-none"><i class="fal fa-bars"></i></button>
                         </div>
                         <div class="col-auto d-none d-lg-block">
-                            <div class="header-btns"><a href="#" class="vs-btn d-none d-xxl-inline-block">Contact Us<i
+                            <div class="header-btns"><a href="#" class="vs-btn d-none d-xxl-inline-block">Commande<i
                                         class="far fa-arrow-right"></i></a>
                                 <button class="icon-btn style3 sideMenuToggler"><i class="far fa-bars"></i></button>
                             </div>
