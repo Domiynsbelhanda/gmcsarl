b5fbf2ab690447c80cb42cc3096a35c346bda335
Youness Dominique <youness.dominique@gmail.com>
2023-04-26 10:12:54 +0200

    [FEAT] Contact page and information

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -18,193 +18,72 @@
 
     <section class="space-top space-extra-bottom">
         <div class="container">
-            <div class="nav contact-tab-menu" id="nav-contactTab" role="tablist">
-                <button class="nav-link active" id="nav-GermanyAddress-tab" data-bs-toggle="tab"
-                        data-bs-target="#nav-GermanyAddress" type="button" role="tab" aria-controls="nav-GermanyAddress"
-                        aria-selected="true"><span class="btn-img"><img src="assets/img/contact/contact-1-1.jpg"
-                                                                        alt="contact"></span><span class="btn-title">TechBiz, Germany</span>
-                    <span class="btn-text">86 KKL, FrankFrut</span></button>
-                <button class="nav-link" id="nav-AustraliaAddress-tab" data-bs-toggle="tab"
-                        data-bs-target="#nav-AustraliaAddress" type="button" role="tab" aria-controls="nav-AustraliaAddress"
-                        aria-selected="false"><span class="btn-img"><img src="assets/img/contact/contact-1-2.jpg"
-                                                                         alt="contact"></span><span class="btn-title">TechBiz, Australia</span>
-                    <span class="btn-text">259 NYD, Canberra</span></button>
-                <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact"
-                        type="button" role="tab" aria-controls="nav-contact" aria-selected="false"><span
-                        class="btn-img"><img src="assets/img/contact/contact-1-3.jpg" alt="contact"></span><span
-                        class="btn-title">TechBiz, United State</span> <span class="btn-text">259 NYD, NewYork</span>
-                </button>
-            </div>
             <div class="tab-content" id="nav-contactTabContent">
                 <div class="tab-pane fade show active" id="nav-GermanyAddress" role="tabpanel"
                      aria-labelledby="nav-GermanyAddress-tab">
                     <div class="row">
                         <div class="col-lg-6 mb-30">
-                            <div class="contact-box"><h3 class="contact-box__title h4">Germany Office Address</h3>
-                                <p class="contact-box__text">Completely recaptiualize 24/7 communities via standards
-                                    compliant metrics whereas web-enabled content</p>
+                            <div class="contact-box"><h3 class="contact-box__title h4">Nos bureaux</h3>
+                                <p class="contact-box__text">
+                                    _____________________________________
+                                </p>
                                 <div class="contact-box__item">
                                     <div class="contact-box__icon"><i class="fal fa-phone-alt"></i></div>
-                                    <div class="media-body"><h4 class="contact-box__label">Phone Number & Email</h4>
-                                        <p class="contact-box__info"><a href="tel:+310259121563">+(310) 2591 21563</a><a
-                                                href="mailto:info@example.com">info@example.com</a></p></div>
+                                    <div class="media-body"><h4 class="contact-box__label">N° Téléphone et Adresse email</h4>
+                                        <p class="contact-box__info">
+                                            <a href="tel:+243811497046"> +243811497046</a>
+                                            <a href="tel:+243972043209">+243972043209</a>
+                                            <a href="mailto:mannecachee2007@yahoo.fr">mannecachee2007@yahoo.fr</a>
+                                        </p>
+                                    </div>
                                 </div>
                                 <div class="contact-box__item">
                                     <div class="contact-box__icon"><i class="far fa-map-marker-alt"></i></div>
-                                    <div class="media-body"><h4 class="contact-box__label">Our Office Address</h4>
-                                        <p class="contact-box__info">258 Dancing Street, Miland Line, HUYI 21563,
-                                            FrankFrut</p></div>
+                                    <div class="media-body"><h4 class="contact-box__label">Office Address</h4>
+                                        <p class="contact-box__info">
+                                            50, Avenue Manne Cachée, Golf TSHAMALALE, Lubumbashi
+                                        </p>
+                                    </div>
                                 </div>
                                 <div class="contact-box__item">
                                     <div class="contact-box__icon"><i class="far fa-clock"></i></div>
-                                    <div class="media-body"><h4 class="contact-box__label">Official Work Time</h4>
-                                        <p class="contact-box__info">7:00am - 6:00pm ( Mon - Fri ) Sat, Sun & Holiday
-                                            Closed</p></div>
+                                    <div class="media-body"><h4 class="contact-box__label">Heure de travail</h4>
+                                        <p class="contact-box__info">
+                                            8:00 - 17:00 ( Lundi - Vendredi ),
+                                            8:00 - 12:00 (Samedi),
+                                            Fermé (Dimanche)
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <div class="col-lg-6 mb-30">
-                            <div class="contact-box"><h3 class="contact-box__title h4">Leave a Message</h3>
-                                <p class="contact-box__text">We’re Ready To Help You</p>
+                            <div class="contact-box"><h3 class="contact-box__title h4">Laissez-nous un message</h3>
+                                <p class="contact-box__text">Nous sommes prêt pour vous aider.</p>
                                 <form class="contact-box__form ajax-contact"
-                                      action="https://html.vecurosoft.com/techbiz/demo/mail.php" method="POST">
+                                      action="#" method="POST">
                                     <div class="row gx-20">
-                                        <div class="col-md-6 form-group"><input type="text" name="name" id="name"
-                                                                                placeholder="Your Name"> <i
+                                        <div class="col-md-6 form-group">
+                                            <input type="text" name="name" id="name"
+                                                                                placeholder="Votre nom"> <i
                                                 class="fal fa-user"></i></div>
-                                        <div class="col-md-6 form-group"><input type="email" name="email" id="email"
-                                                                                placeholder="Email Address"> <i
+                                        <div class="col-md-6 form-group">
+                                            <input type="email" name="email" id="email"
+                                                                                placeholder="Adresse email"> <i
                                                 class="fal fa-envelope"></i></div>
                                         <div class="col-12 form-group"><select name="subject" id="subject">
-                                                <option selected="selected" disabled="disabled" hidden>Select subject</option>
-                                                <option value="Web Development">Web Development</option>
-                                                <option value="UI Design">UI Design</option>
-                                                <option value="CMS Development">CMS Development</option>
-                                                <option value="Theme Development">Theme Development</option>
-                                                <option value="Wordpress Development">Wordpress Development</option>
+                                                <option selected="selected" disabled="disabled" hidden>Sujet:</option>
+                                                <option value="Web Development">Eau minerale</option>
+                                                <option value="UI Design">Imprimerie</option>
+                                                <option value="CMS Development">Publicité</option>
+                                                <option value="Theme Development">Autres</option>
                                             </select></div>
-                                        <div class="col-12 form-group"><textarea name="message" id="message"
-                                                                                 placeholder="Type Your Message"></textarea>
+                                        <div class="col-12 form-group">
+                                            <textarea name="message" id="message"
+                                                                                 placeholder="Entrez votre message ici"></textarea>
                                         </div>
                                         <div class="col-12">
-                                            <button class="vs-btn">Submit Message<i class="far fa-arrow-right"></i></button>
-                                        </div>
-                                    </div>
-                                </form>
-                                <p class="form-messages mb-0 mt-3"></p></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="tab-pane fade" id="nav-AustraliaAddress" role="tabpanel"
-                     aria-labelledby="nav-AustraliaAddress-tab">
-                    <div class="row">
-                        <div class="col-lg-6 mb-30">
-                            <div class="contact-box"><h3 class="contact-box__title h4">Australia Office Address</h3>
-                                <p class="contact-box__text">Completely recaptiualize 24/7 communities via standards
-                                    compliant metrics whereas web-enabled content</p>
-                                <div class="contact-box__item">
-                                    <div class="contact-box__icon"><i class="fal fa-phone-alt"></i></div>
-                                    <div class="media-body"><h4 class="contact-box__label">Phone Number & Email</h4>
-                                        <p class="contact-box__info"><a href="tel:+310259121563">+(310) 2591 21563</a><a
-                                                href="mailto:info@example.com">info@example.com</a></p></div>
-                                </div>
-                                <div class="contact-box__item">
-                                    <div class="contact-box__icon"><i class="far fa-map-marker-alt"></i></div>
-                                    <div class="media-body"><h4 class="contact-box__label">Our Office Address</h4>
-                                        <p class="contact-box__info">258 Dancing Street, Miland Line, HUYI 21563,
-                                            Canberra</p></div>
-                                </div>
-                                <div class="contact-box__item">
-                                    <div class="contact-box__icon"><i class="far fa-clock"></i></div>
-                                    <div class="media-body"><h4 class="contact-box__label">Official Work Time</h4>
-                                        <p class="contact-box__info">7:00am - 6:00pm ( Mon - Fri ) Sat, Sun & Holiday
-                                            Closed</p></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 mb-30">
-                            <div class="contact-box"><h3 class="contact-box__title h4">Leave a Message</h3>
-                                <p class="contact-box__text">We’re Ready To Help You</p>
-                                <form class="contact-box__form ajax-contact2"
-                                      action="https://html.vecurosoft.com/techbiz/demo/mail.php" method="POST">
-                                    <div class="row gx-20">
-                                        <div class="col-md-6 form-group"><input type="text" name="name" id="name2"
-                                                                                placeholder="Your Name"> <i
-                                                class="fal fa-user"></i></div>
-                                        <div class="col-md-6 form-group"><input type="email" name="email" id="email2"
-                                                                                placeholder="Email Address"> <i
-                                                class="fal fa-envelope"></i></div>
-                                        <div class="col-12 form-group"><select name="subject" id="subject2">
-                                                <option selected="selected" disabled="disabled" hidden>Select subject</option>
-                                                <option value="Web Development">Web Development</option>
-                                                <option value="UI Design">UI Design</option>
-                                                <option value="CMS Development">CMS Development</option>
-                                                <option value="Theme Development">Theme Development</option>
-                                                <option value="Wordpress Development">Wordpress Development</option>
-                                            </select></div>
-                                        <div class="col-12 form-group"><textarea name="message" id="message2"
-                                                                                 placeholder="Type Your Message"></textarea>
-                                        </div>
-                                        <div class="col-12">
-                                            <button class="vs-btn">Submit Message<i class="far fa-arrow-right"></i></button>
-                                        </div>
-                                    </div>
-                                </form>
-                                <p class="form-messages mb-0 mt-3"></p></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
-                    <div class="row">
-                        <div class="col-lg-6 mb-30">
-                            <div class="contact-box"><h3 class="contact-box__title h4">United State Office Address</h3>
-                                <p class="contact-box__text">Completely recaptiualize 24/7 communities via standards
-                                    compliant metrics whereas web-enabled content</p>
-                                <div class="contact-box__item">
-                                    <div class="contact-box__icon"><i class="fal fa-phone-alt"></i></div>
-                                    <div class="media-body"><h4 class="contact-box__label">Phone Number & Email</h4>
-                                        <p class="contact-box__info"><a href="tel:+310259121563">+(310) 2591 21563</a><a
-                                                href="mailto:info@example.com">info@example.com</a></p></div>
-                                </div>
-                                <div class="contact-box__item">
-                                    <div class="contact-box__icon"><i class="far fa-map-marker-alt"></i></div>
-                                    <div class="media-body"><h4 class="contact-box__label">Our Office Address</h4>
-                                        <p class="contact-box__info">258 Dancing Street, Miland Line, HUYI 21563,
-                                            NewYork</p></div>
-                                </div>
-                                <div class="contact-box__item">
-                                    <div class="contact-box__icon"><i class="far fa-clock"></i></div>
-                                    <div class="media-body"><h4 class="contact-box__label">Official Work Time</h4>
-                                        <p class="contact-box__info">7:00am - 6:00pm ( Mon - Fri ) Sat, Sun & Holiday
-                                            Closed</p></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 mb-30">
-                            <div class="contact-box"><h3 class="contact-box__title h4">Leave a Message</h3>
-                                <p class="contact-box__text">We’re Ready To Help You</p>
-                                <form class="contact-box__form ajax-contact3"
-                                      action="https://html.vecurosoft.com/techbiz/demo/mail.php" method="POST">
-                                    <div class="row gx-20">
-                                        <div class="col-md-6 form-group"><input type="text" name="name" id="name3"
-                                                                                placeholder="Your Name"> <i
-                                                class="fal fa-user"></i></div>
-                                        <div class="col-md-6 form-group"><input type="email" name="email" id="email3"
-                                                                                placeholder="Email Address"> <i
-                                                class="fal fa-envelope"></i></div>
-                                        <div class="col-12 form-group"><select name="subject" id="subject3">
-                                                <option selected="selected" disabled="disabled" hidden>Select subject</option>
-                                                <option value="Web Development">Web Development</option>
-                                                <option value="UI Design">UI Design</option>
-                                                <option value="CMS Development">CMS Development</option>
-                                                <option value="Theme Development">Theme Development</option>
-                                                <option value="Wordpress Development">Wordpress Development</option>
-                                            </select></div>
-                                        <div class="col-12 form-group"><textarea name="message" id="message3"
-                                                                                 placeholder="Type Your Message"></textarea>
-                                        </div>
-                                        <div class="col-12">
-                                            <button class="vs-btn">Submit Message<i class="far fa-arrow-right"></i></button>
+                                            <button class="vs-btn">Soumettre<i class="far fa-arrow-right"></i></button>
                                         </div>
                                     </div>
                                 </form>
